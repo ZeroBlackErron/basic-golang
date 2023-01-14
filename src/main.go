bd619b0ca2888c9b3293e196ac5f962e55873bd9
Anthony Rosado <arosado-26@hotmail.com>
2023-01-14 16:36:20 -0500

    Hello world

---
+++ src/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+}
+
+// go build: create a new file to execute the program
+// go run: similar to `go build`, but with a temporary file. Better for development
